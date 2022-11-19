deaf058c98bea116ac71ee4eb3962b51153c8efb
thara0000 <anuradha.spta@gmail.com>
2022-11-20 05:18:29 +0530

    as

--- resources/js/Pages/Students/Create.vue
+++ resources/js/Pages/Students/Create.vue
@@ -49,7 +49,7 @@ const form = useForm({
                                     <div class="mt-1">
                                         <select id="status" v-model="form.status" wire:model.lazy="status" name="status" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5">
                                             <option>Active</option>
-                                            <option>InaActive</option>
+                                            <option>Inactive</option>
                                         </select>
                                         <!-- <input type="text" id="title" wire:model.lazy="title" name="title" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" /> -->
                                     </div>
